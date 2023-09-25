2de5c4c1b1f153a422a306212ca1ff7a62b29173
gnikoloff <nikoloffgeorgi@gmail.com>
2023-09-25 15:16:27 +0200

    add og image

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("eleventy-src/style.css");
 	eleventyConfig.addPassthroughCopy("eleventy-src/script.js");
+	eleventyConfig.addPassthroughCopy({
+		"previews/geometry-demo.png": "og-image.png"
+	});
   // Set custom directories for input, output, includes, and data
   return {
 		passthroughFileCopy: true,
